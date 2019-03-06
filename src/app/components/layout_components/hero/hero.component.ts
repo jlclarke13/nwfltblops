33620161ce621d47d9cfb7662216b3d8f54d1caf
Joshua Clarke <josh.clarke11@gmail.com>
2019-03-05 23:29:51 -0500

    cubic bezier adjustment for scroll button on header as well as offset adjustment

--- src/app/components/layout_components/hero/hero.component.ts
+++ src/app/components/layout_components/hero/hero.component.ts
@@ -12,21 +12,21 @@ export class HeroComponent implements OnInit {
 	@Input() innerBgClass: string;
 
 	public myEasing: EasingLogic = (t: number, b: number, c: number, d: number): number => {
-      // easeInOutExpo easing
-      if (t === 0) {
-        return b;
-      }
-      if (t === d) {
-        return b + c;
-      }
-      if ((t /= d / 2) < 1) {
-        //return c / 2 * Math.pow(2, 10 * (t - 1)) + b; // original
-        return c / 2 * Math.pow(2, 6 * (t - 1)) + b;
-      }
-  
-      //return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b; // original
-      return c / 2 * (-Math.pow(2, -6 * --t) + 2) + b;
+    // easeInOutExpo easing
+    if (t === 0) {
+      return b;
     }
+    if (t === d) {
+      return b + c;
+    }
+    if ((t /= d / 2) < 1) {
+      //return c / 2 * Math.pow(2, 10 * (t - 1)) + b; // original
+      return c / 2 * Math.pow(2, 7 * (t - 1)) + b;
+    }
+
+    //return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b; // original
+    return c / 2 * (-Math.pow(2, -7 * --t) + 2) + b;
+  }
 
   constructor() { }
 
